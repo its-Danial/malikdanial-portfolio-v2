ead8dd6600ae29b98703f9b08b533c470b79c38e
its-Danial <itsmalikdanial@gmail.com>
2024-06-28 16:35:14 +0500

    created footer

---
+++ src/components/section/Footer.tsx
@@ -0,0 +1,69 @@
+export default function Footer() {
+  return (
+    <footer className="max-w-md pb-16 text-sm text-slate-500 sm:pb-0">
+      <p>
+        Coded in{" "}
+        <a
+          href="https://code.visualstudio.com/"
+          className="font-medium text-slate-400 hover:text-teal-300 focus-visible:text-teal-300"
+          target="_blank"
+          rel="noreferrer noopener"
+          aria-label="Visual Studio Code (opens in a new tab)"
+        >
+          Visual Studio Code
+        </a>{" "}
+        Built with{" "}
+        <a
+          href="https://nextjs.org/"
+          className="font-medium text-slate-400 hover:text-teal-300 focus-visible:text-teal-300"
+          target="_blank"
+          rel="noreferrer noopener"
+          aria-label="Next.js (opens in a new tab)"
+        >
+          Next.js
+        </a>
+        {", "}
+        <a
+          href="https://www.sanity.io/"
+          className="font-medium text-slate-400 hover:text-teal-300 focus-visible:text-teal-300"
+          target="_blank"
+          rel="noreferrer noopener"
+          aria-label="Next.js (opens in a new tab)"
+        >
+          Sanity
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://tailwindcss.com/"
+          className="font-medium text-slate-400 hover:text-teal-300 focus-visible:text-teal-300"
+          target="_blank"
+          rel="noreferrer noopener"
+          aria-label="Tailwind CSS (opens in a new tab)"
+        >
+          Tailwind CSS
+        </a>
+        , deployed with{" "}
+        <a
+          href="https://vercel.com/"
+          className="font-medium text-slate-400 hover:text-teal-300 focus-visible:text-teal-300"
+          target="_blank"
+          rel="noreferrer noopener"
+          aria-label="Vercel (opens in a new tab)"
+        >
+          Vercel
+        </a>
+        . All text is set in the{" "}
+        <a
+          href="https://rsms.me/inter/"
+          className="font-medium text-slate-400 hover:text-teal-300 focus-visible:text-teal-300"
+          target="_blank"
+          rel="noreferrer noopener"
+          aria-label="Inter (opens in a new tab)"
+        >
+          Inter
+        </a>{" "}
+        typeface.
+      </p>
+    </footer>
+  );
+}
