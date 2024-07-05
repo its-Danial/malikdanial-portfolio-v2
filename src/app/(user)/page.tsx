17ae7ee0b592a16eb96e9ae1020332ccfd943436
its-Danial <itsmalikdanial@gmail.com>
2024-07-05 20:48:29 +0500

    use header component fetch data from cms

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -1,38 +1,25 @@
 import type { Metadata } from "next";
-import Link from "next/link";
 
-import Nav from "@/components/nav";
-import SocialLinks from "@/components/nav/SocialLinks";
 import About from "@/components/section/About";
 import Experience from "@/components/section/Experience";
 import Footer from "@/components/section/Footer";
+import Header from "@/components/section/Header";
 import Projects from "@/components/section/Projects";
+import { getMyInfo } from "@/lib/queries";
 
 export const metadata: Metadata = {
   title: "Danial | Software Engineer",
   description: "Portfolio website for Malik Danial",
 };
 
-export default function Home() {
+export default async function Home() {
+  const { firstName, jobTitle, tagline, summary } = await getMyInfo();
+
   return (
     <div className="lg:flex lg:justify-between lg:gap-4">
-      <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24">
-        <div>
-          <h1 className="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl">
-            <Link href="/">Malik Danial</Link>
-          </h1>
-          <h2 className="mt-3 text-lg font-medium tracking-tight text-slate-200 sm:text-xl">
-            Software Engineer II
-          </h2>
-          <p className="mt-4 max-w-xs leading-normal">
-            I build engaging and accessible digital experiences.
-          </p>
-          <Nav />
-        </div>
-        <SocialLinks />
-      </header>
+      <Header firstName={firstName} jobTitle={jobTitle} tagline={tagline} />
       <main className="pt-24 lg:w-1/2 lg:py-24">
-        <About />
+        <About summary={summary} />
         <Experience />
         <Projects />
         <Footer />
