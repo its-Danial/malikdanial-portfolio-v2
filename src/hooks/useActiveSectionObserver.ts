cb6ce4e4d13eb505d3a89f9b05e999f83aeebf98
its-Danial <itsmalikdanial@gmail.com>
2024-07-07 15:25:57 +0500

    update threshold

--- src/hooks/useActiveSectionObserver.ts
+++ src/hooks/useActiveSectionObserver.ts
@@ -7,7 +7,7 @@ export function useActiveSectionObserver(sectionIds: string[]) {
     const observerOptions = {
       root: null,
       rootMargin: "0px",
-      threshold: [0.75],
+      threshold: 0.6,
     };
 
     const observerCallback = (entries: IntersectionObserverEntry[]) => {
