8cd30f34d7900c1b2d99cb9367454b34b4386cfd
its-Danial <itsmalikdanial@gmail.com>
2024-07-07 15:17:18 +0500

    add sanity host

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "www.malikdanial.xyz",
+        hostname: "cdn.sanity.io",
         port: "",
       },
     ],
